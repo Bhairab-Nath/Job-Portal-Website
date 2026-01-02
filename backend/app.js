e486be2ed09ba5592e6b89f5d2c8432cf60c62f1
Bhairab-Nath <nathbhairab22@gmail.com>
2026-01-02 22:46:56 +0545

    Supabase Database connection done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
 const express = require('express');
+const { connectDB } = require('./db/dbConfig');
 const app = express();
+
+app.use(express.json()) //frontend ko data backend lai bujhauna yo use garne
+
+connectDB();
 
 app.get("/",(req,res)=>{
     res.send("hello world");
@@ -8,6 +14,18 @@ app.get("/",(req,res)=>{
 app.get("/home",(req,res)=>{
     res.send("This is home page.");
 })
+
+app.get("/about",(req,res)=>{
+    res.send("This is about page.");
+})
+
+app.post("/hello", (req,res)=>{
+    console.log(req.body)
+    res.send("Post request done.")
+})
+
+
+
 
 app.listen(4000, ()=>{
     console.log("Server is running on port 4000");  

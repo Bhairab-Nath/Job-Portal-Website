598a6940b060e8e654643d50eaaf26b13fd87631
Bhairab-Nath <nathbhairab22@gmail.com>
2026-01-07 23:03:28 +0545

    small change

--- backend/routes/jobRoute.js
+++ backend/routes/jobRoute.js
@@ -5,7 +5,7 @@ const router = require("express").Router()
 // router.post("/createJob",authMiddleware,createJob)
 // router.get("/getAllJobs",getAllJobs)
 
-router.route("").get(getAllJobs).post(authMiddleware,createJob)
+router.route("/").get(getAllJobs).post(authMiddleware,createJob)
 router.route("/:id").get(getSingleJob).patch(updateJob).delete(deleteJob)
 
 module.exports = router

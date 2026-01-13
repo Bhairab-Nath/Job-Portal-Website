faef7207ca942203d6efebcf03587e5590fe0a80
Bhairab-Nath <nathbhairab22@gmail.com>
2026-01-13 21:04:01 +0545

    Bug Fix

--- backend/model/index.js
+++ backend/model/index.js
@@ -1,5 +1,5 @@
 const Job = require("./jobModel");
-const User = require("./UserModel");
+const User = require("./userModel")
 
 User.hasMany(Job, { foreignKey: 'userId'});
 Job.belongsTo(User, { foreignKey: 'userId'});

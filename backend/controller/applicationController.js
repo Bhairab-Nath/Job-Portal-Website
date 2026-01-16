d420c944caf6b22e69a5362758291dd85be529cd
Bhairab-Nath <nathbhairab22@gmail.com>
2026-01-16 22:35:05 +0545

    applicationController done

--- backend/controller/applicationController.js
+++ backend/controller/applicationController.js
@@ -1,7 +1,7 @@
-const Application  = require("../model/applicationModel")
+const Application = require("../model/applicationModel")
 
-const jobApply = async(req,res)=>{
-    const {jobId} = req.params
+const jobApply = async (req, res) => {
+    const { jobId } = req.params
     const userId = req.user.id
 
     const application = await Application.create({
@@ -9,12 +9,102 @@ const jobApply = async(req,res)=>{
         jobId
     })
 
-    res.status(201).json({
+    res.status(200).json({
         message: "Job applied successfully!",
         application
     })
 
 }
 
+const getAllApplications = async (req, res) => {
 
-module.exports = {jobApply}
+    const applications = await Application.findAll()
+
+    if (applications.length === 0) {
+        return res.status(404).json({
+            message: "Applications not found!"
+        })
+    }
+
+    res.status(200).json({
+        message: "Applications fetched successfully!",
+        applications
+    })
+}
+
+const myApplication = async (req, res) => {
+    const { id } = req.user
+    const applications = await Application.findAll({
+        where: {
+            userId: id
+        }
+    })
+
+    if (applications.length === 0) {
+        return res.status(404).json({
+            message: "Applications not found!"
+        })
+    }
+
+    res.status(200).json({
+        message: "Applications fetched successfully!",
+        applications
+    })
+
+}
+
+const updateApplicationStatus = async (req, res) => {
+    const { id } = req.params
+    const { status } = req.body
+
+    const application = await Application.findByPk(id)
+
+    if (!application) {
+        return res.status(404).json({
+            message: "Application not found!"
+        })
+    }
+
+    const [count, updatedApplication] = await Application.update({
+        status
+    }, {
+        where: {
+            id: id
+        },
+        returning: true
+    })
+
+    res.status(200).json({
+        message: "Application Status updated successfully!",
+        updatedApplication
+    })
+
+}
+
+
+const deleteApplication = async (req, res) => {
+    const { id } = req.params
+    const application = await Application.findByPk(id)
+
+    if (!application) {
+        return res.status(404).json({
+            message: "Application not found!"
+        })
+    }
+    
+    await Application.destroy({
+        where: {
+            id: id
+        }
+    })
+
+    res.status(200).json({
+        message:"Application deleted successfully!"
+    })
+
+}
+
+
+
+
+module.exports = { jobApply, getAllApplications, myApplication, updateApplicationStatus, deleteApplication }
--- backend/routes/applicationRoute.js
+++ backend/routes/applicationRoute.js
@@ -1,9 +1,16 @@
-const { jobApply } = require("../controller/applicationController");
+const { jobApply, updateApplicationStatus, deleteApplication, getAllApplications, myApplication } = require("../controller/applicationController");
 const authenticate = require("../middlewares/authMiddleware");
 const roleAuthorised = require("../middlewares/roleMiddleware");
 const catchError = require("../services/catchError");
 const router = require("express").Router();
 
-router.route("/:jobId").post(authenticate, roleAuthorised("jobseeker"), catchError(jobApply))
+router.route("/apply/:jobId").post(authenticate, roleAuthorised("jobseeker"), catchError(jobApply))
+
+router.route("/").get(authenticate,roleAuthorised("jobprovider"),catchError(getAllApplications))
+
+router.route("/myapplications").get(authenticate,roleAuthorised("jobseeker"),catchError(myApplication))
+
+router.route("/:id").patch(authenticate,roleAuthorised("jobprovider"),catchError(updateApplicationStatus)).delete(authenticate,roleAuthorised("jobprovider"),catchError(deleteApplication))
+
 
 module.exports = router;
